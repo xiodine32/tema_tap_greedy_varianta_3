9378c5b824d170481736fe249081ada3ca6ee207
Neagu Rares <neagu1000@gmail.com>
2015-11-22 20:28:23 +0200

    Location now working

--- src/com/xiodine/teme/tap/greedy/varianta3/Main.java
+++ src/com/xiodine/teme/tap/greedy/varianta3/Main.java
@@ -2,100 +2,105 @@ package com.xiodine.teme.tap.greedy.varianta3;
 
 import com.xiodine.teme.tap.greedy.varianta3.helpers.Menu;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.lang.reflect.Constructor;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        while (true) {
-            // create problem picker
-            Menu menuHelper = Menu.factory("Pick exercise number", new String[]{"Problem 1", "Problem 2", "Problem 3", "exit"});
-
-            // pick problem
-            menuHelper.run(System.out, new Scanner(System.in));
-
-            // get answer
-            int problema = menuHelper.getAnswer();
-
-            if (problema == 3)
-                break;
-
-            // construct class
-            Class<?> selectedClass = null;
-            try {
-                selectedClass = Class.forName("com.xiodine.teme.tap.greedy.varianta3.problema" + problema + ".Main");
-            } catch (Exception e) {
-                System.err.println("Error loading problem!");
-            }
-            if (selectedClass == null)
-                continue;
-
-            Constructor constructor = null;
-            try {
-                constructor = selectedClass.getConstructor();
-            } catch (Exception e) {
-                System.err.println("Error getting constructor for the problem!");
-            }
-
-            if (constructor == null)
-                continue;
-
-
-            // start loading redirects
-            InputStream in = selectedClass.getResourceAsStream("/input.in");
-
-            if (in == null) {
-                System.err.print("Input file not found!");
-                continue;
-            }
-
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-            // save old redirects
-            InputStream oldIn = System.in;
-            PrintStream oldOut = System.out;
-
-
-            // redirect
-            System.setOut(new PrintStream(out));
-            System.setIn(in);
-
-            // prepare timer
-            NanoTimer timer = new NanoTimer();
-            timer.setLambda(constructor);
-
-            // run test
-            try {
-                timer.runTest();
-            } catch (Exception e) {
-                System.err.println("Uh oh, program crashed!");
-                e.printStackTrace();
-            }
-
-            // get output
-            String result = null;
-            try {
-                result = out.toString("UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                System.err.println("Result cannot be encoded to UTF-8.");
-            }
-
-
-            // restore redirects
-            System.setOut(oldOut);
-            System.setIn(oldIn);
-
-
-            // write to output
-            System.out.println("Result: " + result);
-            System.out.printf("Time elapsed: %.5fs\n", timer.getTimerDuration() / 1000000000.0);
-            System.out.println();
+        boolean result = true;
+        while (result) {
+            result = (new Main()).run();
         }
         System.out.println("Goodbye!");
+    }
+
+    private boolean run() {
+        // create problem picker
+        Menu menuHelper = Menu.factory("Pick exercise number", new String[]{"Problem 1", "Problem 2", "Problem 3", "exit"});
+
+        // pick problem
+        menuHelper.run(System.out, new Scanner(System.in));
+
+        // get answer
+        int problema = menuHelper.getAnswer();
+
+        if (problema == 3)
+            return false;
+
+        // construct class
+        Class<?> selectedClass = null;
+        try {
+            selectedClass = Class.forName("com.xiodine.teme.tap.greedy.varianta3.problema" + problema + ".Main");
+        } catch (Exception e) {
+            System.err.println("Error loading problem!");
+        }
+        if (selectedClass == null)
+            return true;
+
+        Constructor constructor = null;
+        try {
+            constructor = selectedClass.getConstructor();
+        } catch (Exception e) {
+            System.err.println("Error getting constructor for the problem!");
+        }
+
+        if (constructor == null)
+            return true;
+
+
+        // start loading redirects
+        String location = "src/com/xiodine/teme//tap/greedy/varianta3/problema" + problema + "/input.in";
+        InputStream in;
+        try {
+            in = new FileInputStream(location);
+        } catch (FileNotFoundException e) {
+            System.err.println("Location not found: " + location);
+            return true;
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        // save old redirects
+        InputStream oldIn = System.in;
+        PrintStream oldOut = System.out;
+
+
+        // redirect
+        System.setOut(new PrintStream(out));
+        System.setIn(in);
+
+        // prepare timer
+        NanoTimer timer = new NanoTimer();
+        timer.setLambda(constructor);
+
+        // run test
+        try {
+            timer.runTest();
+        } catch (Exception e) {
+            System.err.println("Uh oh, program crashed!");
+            e.printStackTrace();
+        }
+
+        // get output
+        String result = null;
+        try {
+            result = out.toString("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("Result cannot be encoded to UTF-8.");
+        }
+
+
+        // restore redirects
+        System.setOut(oldOut);
+        System.setIn(oldIn);
+
+
+        // write to output
+        System.out.println("Result: " + result);
+        System.out.printf("Time elapsed: %.5fs\n", timer.getTimerDuration() / 1000000000.0);
+        System.out.println();
+        return true;
     }
 }
